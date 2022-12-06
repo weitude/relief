73564e9f609b42539693724ead93db11b18ee402
terry489490 <terry489490@gmail.com>
2022-12-06 08:12:25 +0800

    create user dope

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -5,10 +5,10 @@ const  Schema  = mongoose.Schema
 const UserSchema = new Schema({
     name: { type: String, required: [true, 'Name field is required.'] },
     email: { type: String, required: [true, 'Email field is required.'] },
-    password: { type: String, required: [true, 'Password field is required.']},
+    hash: { type: String, required: [true, 'Password field is required.']},
     rule: { type: String, required: [true, 'Rule field is required.'] }
 })
 
-const UserModel = mongoose.model('ChatBox', UserSchema);
+const User = mongoose.model('User', UserSchema);
 
-export  { UserModel }
+export  { User }
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,29 @@
 import express from 'express'
+import { sha256 } from 'js-sha256';
+import { User } from '../models/user'
 
 const router = express.Router()
 
-router.post('/test', (req, res) => {
-	console.log("testsetsetststase")
+const CreateUser = async (res, name, email, hash, rule) => {
+	console.log(name)
+	try{
+		User.create({name: name, email: email, hash: hash, rule: rule})
+		res.json({message: "success"})
+	}
+	catch (e) {
+		res.json({message: e})
+	}
+}
+
+router.post('/login', (req, res) => {
+	let name = req.body.name
+	let email = req.body.email
+	let password = req.body.password
+	let rule = req.body.rule
+
+	var hash = sha256(password);
+	
+	CreateUser(res, name, email, hash, rule)
 })
 
 export default router
