5db00eb00dce5e48764591fcc7bd80b7f325cb72
weitude <eric.c.w.lin@gmail.com>
2022-12-29 20:20:57 +0800

    fix baseURL

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:4040/api/",
-    // baseURL: "https://nturelief.up.railway.app/api/",
+    // baseURL: "http://localhost:4040/api/",
+    baseURL: "https://nturelief.up.railway.app/api/",
 });
 
 const API_signin = async (name, password) => {
