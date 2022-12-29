79f029c7b714400436c511ae8e3c1d1e9e9a20b4
terry489490 <terry489490@gmail.com>
2022-12-29 15:57:39 +0800

    QQ

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -7,8 +7,15 @@ import { Card } from "../models/Card.js";
 
 const router = express.Router();
 
-const CreateUser = async (res, name, email, hash, role) => {
+router.post("/signup", async (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+  const role = req.body.role;
+  const hash = sha256(password);
+
   const result = await User.find({ name: name });
+
   if (result.length >= 1) {
     res.json({ message: "Username exist", type: 2 });
     return;
@@ -19,13 +26,15 @@ const CreateUser = async (res, name, email, hash, role) => {
   } catch (e) {
     res.json({ message: "error", type: 0 });
   }
-};
+});
 
-const Login = async (res, name, hash) => {
+router.get("/signin", async (req, res) => {
+  const name = req.query.name;
+  const password = req.query.password;
+  const hash = sha256(password);
   const result = await User.find({ name: name });
 
   if (result.length === 1) {
-    console.log(hash);
     if (result[0].hash === hash)
       res.json({ message: "success", content: result, type: 1 });
     else res.json({ message: "Wrong password", content: [], type: 2 });
@@ -34,9 +43,15 @@ const Login = async (res, name, hash) => {
   } else {
     res.json({ message: "Can't find user", content: [], type: 0 });
   }
-};
+});
 
-const PostCard = async (res, title, question, tag, id) => {
+router.post("/postcard", async (req, res) => {
+  const buf = crypto.randomBytes(8);
+  const id = Base64.encode(buf);
+  const title = req.body.title;
+  const question = req.body.question;
+  const tag = [...req.body.tag, "others"];
+
   try {
     const result = await Card.create({
       title: title,
@@ -50,9 +65,12 @@ const PostCard = async (res, title, question, tag, id) => {
   } catch (e) {
     res.json({ message: "error", content: [], type: 0 });
   }
-};
+});
 
-const Reply = async (res, id, response) => {
+router.post("/reply", async (req, res) => {
+  const id = req.body.id;
+  const response = req.body.response;
+
   try {
     const result = await Card.updateOne(
       { id: id },
@@ -62,9 +80,12 @@ const Reply = async (res, id, response) => {
   } catch {
     res.json({ message: "error", content: [], type: 0 });
   }
-};
+});
 
-const SearchByID = async (res, id, isadmin) => {
+router.get("/opencard", async (req, res) => {
+  const isadmin = req.body.isadmin;
+  const id = req.body.id;
+
   try {
     const result = await Card.findOne({ id: id });
     if (result.replied == false && !isadmin)
@@ -73,19 +94,11 @@ const SearchByID = async (res, id, isadmin) => {
   } catch {
     res.json({ message: "error", content: [], type: 0 });
   }
-};
+});
 
-const Init = async (res) => {
-  try {
-    const result = await Card.find({}).limit(66).sort("created_at");
+router.post("/promote", async (req, res) => {
+  const name = req.body.name;
 
-    res.json({ message: "success", content: result, type: 1 });
-  } catch {
-    res.json({ message: "error", content: [], type: 0 });
-  }
-};
-
-const Pormote = async (res, name) => {
   try {
     await User.updateOne({ name: name }, { $set: { role: "admin" } });
 
@@ -93,9 +106,14 @@ const Pormote = async (res, name) => {
   } catch {
     res.json({ message: "error", type: 0 });
   }
-};
+});
 
-const SearchCard = async (res, target, tag, isreply) => {
+router.get("/search", async (req, res) => {
+  const target = req.query.target;
+  let tag = req.query.tag;
+  if (tag === undefined) tag = [];
+  const isreply = req.query.isreply;
+
   try {
     if (tag.length == 0) tag = ["others"];
     if (target.length == 0) target = ".";
@@ -112,74 +130,10 @@ const SearchCard = async (res, target, tag, isreply) => {
       ],
     }).sort("created_at");
 
-    console.log(result);
     res.json({ message: "success", content: result, type: 1 });
   } catch {
     res.json({ message: "error", content: [], type: 0 });
   }
-};
-
-router.post("/signup", (req, res) => {
-  const name = req.body.name;
-  const email = req.body.email;
-  const password = req.body.password;
-  const role = req.body.role;
-
-  const hash = sha256(password);
-
-  CreateUser(res, name, email, hash, role);
-});
-
-router.get("/signin", (req, res) => {
-  const name = req.query.name;
-  const password = req.query.password;
-  console.log(password);
-
-  const hash = sha256(password);
-
-  Login(res, name, hash);
-});
-
-router.post("/postcard", (req, res) => {
-  const buf = crypto.randomBytes(8);
-  const id = Base64.encode(buf);
-
-  const title = req.body.title;
-  const question = req.body.question;
-  const tag = [...req.body.tag, "others"];
-
-  PostCard(res, title, question, tag, id);
-});
-
-router.post("/reply", (req, res) => {
-  const id = req.body.id;
-  const response = req.body.response;
-
-  Reply(res, id, response);
-});
-
-router.get("/opencard", (req, res) => {
-  const isadmin = req.body.isadmin;
-  const id = req.body.id;
-  SearchByID(res, id, isadmin);
-});
-
-router.get("/init", (req, res) => {
-  Init(res);
-});
-
-router.post("/promote", (req, res) => {
-  const name = req.body.name;
-  Pormote(res, name);
-});
-
-router.get("/search", (req, res) => {
-  const target = req.query.target;
-  let tag = req.query.tag;
-  if (tag === undefined) tag = [];
-  const isreply = req.query.isreply;
-
-  SearchCard(res, target, tag, isreply);
 });
 
 router.get("/health", (_, res) => {
