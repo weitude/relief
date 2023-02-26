123e6ba7c4b7a19eff5d2814710c9d55eef5e0d6
weitude <eric.c.w.lin@gmail.com>
2023-02-26 21:42:37 +0800

    change backend from railway to render

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://nturelief.up.railway.app/api/",
+  baseURL: "https://relief-9d69.onrender.com/",
 });
 
 const API_signin = async (name, password) => {
